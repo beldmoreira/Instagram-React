e9534a86e9ecf43ed526ede0b672f1d4be3c8c63
Isabela <isabela_cmoreira@hotmail.com>
2021-12-04 19:22:05 -0300

    Criação do componente User

---
+++ src/User.js
@@ -0,0 +1,12 @@
+export default function User (){
+    return(
+
+<div class="usuario">
+            <img src="assets/img/catanacomics.svg" />
+            <div class="texto">
+              <strong>catanacomics</strong>
+              Catana
+            </div>
+          </div>
+           );
+        }
