eb845a9e4b6a8362d6bca2d3cec738da26ecd1b7
Isabela <isabela_cmoreira@hotmail.com>
2021-12-04 20:31:59 -0300

    Criação do componente Sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,11 @@
+import User from './User';
+import Suggestions from "./Suggestions";
+import Footer from './Footer';
+
+export default function Sidebar(){
+    return(
+      <User/>
+      <Suggestions/>
+      <Footer/>
+    );
+}
