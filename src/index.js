cdc5f9cb69699fb3c9f0800ea0169c9443ef9223
Isabela <isabela_cmoreira@hotmail.com>
2021-12-04 19:18:39 -0300

    Adição de componentes aO ARQUIVO INDEX

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import ReactDOM from 'react-dom';
 import Header from './Header';
 import Stories from './Stories';
 import Corpo from './Corpo';
+import FirstFile from './FirstFile';
 
 function App() {
+    <div class="root">
+       
+    </div>
     return (
         <Header/>
         <Corpo/>
         <Stories/>
+        <FirstPost/>
+        <SecondPost/>
     );
 }
 const app= App();
