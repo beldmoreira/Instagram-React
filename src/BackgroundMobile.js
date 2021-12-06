ffdb500f76f6849c990bbbc076e9ea22f9594070
Isabela <isabela_cmoreira@hotmail.com>
2021-12-06 14:24:05 -0300

    Componente modificado para se tornar genérico utilizando props

--- src/BackgroundMobile.js
+++ src/BackgroundMobile.js
@@ -1,11 +1,12 @@
-export default function BackgroundMobile(){
-    return(
-    <div class="fundo-mobile">
-        <ion-icon name="home"></ion-icon>
-        <ion-icon name="search-outline"></ion-icon>
-        <ion-icon name="add-circle-outline"></ion-icon>
-        <ion-icon name="heart-outline"></ion-icon>
-        <ion-icon name="person-outline"></ion-icon>
-      </div>
-            );
+export default function BackgroundMobile(props){
+    const iconsNames = ["home","search-outline","add-circle-outline","heart-outline","person-outline"];
+      return(
+        <div class="fundo-mobile">
+            <ion-icon name={props.iconsNames[0]}></ion-icon>
+            <ion-icon name={props.iconsNames[1]}></ion-icon>
+            <ion-icon name={props.iconsNames[2]}></ion-icon>
+            <ion-icon name={props.iconsNames[3]}></ion-icon>
+            <ion-icon name={props.iconsNames[4]}></ion-icon>
+        </div>
+        );
         }
