778efda085115879c7a5fefa4f4d9bdc868dea26
Isabela <isabela_cmoreira@hotmail.com>
2021-12-06 14:30:55 -0300

    Componente genérico em Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,14 @@
 export default function Footer(){
+  const link = "Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma";
     return(
-          <div class="links">
-            Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma
+      <div>
+          <div class= "links">
+           {link}
           </div>
 
           <div class="copyright">
             © 2021 INSTAGRAM DO FACEBOOK
           </div>
-       
+      </div>
       );
     }
