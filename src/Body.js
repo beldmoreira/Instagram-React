d8ebbd0fc61ba4008c6a60438af4c2eb1a7e23cd
Isabela <isabela_cmoreira@hotmail.com>
2021-12-05 21:33:54 -0300

    Alterações na composição do componente Body

--- src/Body.js
+++ src/Body.js
@@ -1,7 +1,14 @@
+import Suggestions from "./Suggestions";
+
 export default function Body(){
 return(
 <div class="corpo">
     <div class="esquerda">
+        <Stories/>
+        <Posts/>
+        <User/>
+        <Suggestions/>
+        <Footer/>
     </div>
 </div>
 );
